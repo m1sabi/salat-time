8efcef024c175d9a2e2a246f76a613cd7781f6df
m1sabi <131496886+m1sabi@users.noreply.github.com>
2026-02-20 06:58:54 +0100

    Add files via upload

---
+++ data/cities/cities-te.js
@@ -0,0 +1,5 @@
+(function () {
+  const C = "te"; // Test
+
+  registerCity("Test_City",       { lat: 48.78, lng:  9.18, alt: 245, label_ar: "مدينة التجربة",       label_en: "test city" }, C);
+})();
