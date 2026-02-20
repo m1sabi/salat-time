4485fd92c02ea2e387857e533ffe2e47fece397f
m1sabi <131496886+m1sabi@users.noreply.github.com>
2026-02-20 07:02:24 +0100

    Delete countries.js

--- countries.js
+++
@@ -1,9 +0,0 @@
-// countries.js
-window.countries = {
-  sy: { label_ar: "سوريا",   label_en: "Syria" },
-  de: { label_ar: "ألمانيا", label_en: "Germany" },
-  se: { label_ar: "السويد",  label_en: "Sweden" },
-  sa: { label_ar: "السعودية", label_en: "Saudi Arabia" },
-  jo: { label_ar: "الأردن",    label_en: "Jordan" },
-  sd: { label_ar: "السودان",   label_en: "Sudan" }
-};
